15528e8efff22f2f00d38c1b39cbb46c5931f7b5
Qi Jiang <114963543+aikojiang@users.noreply.github.com>
2023-04-13 14:09:11 +0800

    Add files via upload

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped lang="scss">
+.q-page-container {
+  background: rgb(251, 203, 103);
+  background: linear-gradient(135deg, rgba(251, 203, 103, 0.7) 0%, rgba(0, 145, 145, 0.7) 50%, rgba(235, 97, 97, 0.7) 100%);
+}
+</style>
