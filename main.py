6eb978792dc3e237b2b6704dd91a1b17aac5a7a7
Qi Jiang <114963543+aikojiang@users.noreply.github.com>
2023-04-13 13:54:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-from scripts.main import main
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/openai-gpt"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+output = query({
+	"inputs": "Can you please let us know more details about your ",
+})
